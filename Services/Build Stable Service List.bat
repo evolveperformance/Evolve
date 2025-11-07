4547cfaa81474246cb8cf3d9d4852a251dc41874
Evolve <valley.oasis1081@eagereverest.com>
2025-11-07 01:46:44 +0100

    fix services

--- Services/Build Stable Service List.bat
+++ Services/Build Stable Service List.bat
@@ -1,12 +1,12 @@
 @echo off
 color 0A
 
-"C:\Ultron\Utilities\Service Builder\service-list-builder.exe" --config "C:\Ultron\Utilities\Service Builder\Stable.ini" --disable-service-warning >nul 2>&1
+"C:\Windows\Evolve\Utilities\Service Builder\service-list-builder.exe" --config "C:\Windows\Evolve\Utilities\Service Builder\Stable.ini" --disable-service-warning >nul 2>&1
 
-copy "C:\Ultron\Utilities\Service Builder\Stable Services.lnk" "C:\" >nul 2>&1
-del "C:\Ultron\Utilities\Service Builder\Stable Services.lnk" >nul 2>&1
+copy "C:\Windows\Evolve\Utilities\Service Builder\Stable Services.lnk" "C:\" >nul 2>&1
+del "C:\Windows\Evolve\Utilities\Service Builder\Stable Services.lnk" >nul 2>&1
 
-explorer "C:\Ultron\Utilities\Service Builder\build"
+explorer "C:\Windows\Evolve\Utilities\Service Builder\build"
 
 echo.
 echo Use NSudoLG.exe to run Services-Disable/Enable.bat scripts
