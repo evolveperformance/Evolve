2be0123f5b42cb98035ecc0ad7d627262afa74bd
Evolve Performance <luckyluca10mc@gmail.com>
2025-11-10 23:52:23 +0100

    Update Build Stable Service List.bat

--- Services/Build Stable Service List.bat
+++ Services/Build Stable Service List.bat
@@ -1,11 +1,16 @@
 @echo off
 color 0A
 
+REM Run the service-list-builder utility
 "C:\Windows\Evolve\Utilities\Service Builder\service-list-builder.exe" --config "C:\Windows\Evolve\Utilities\Service Builder\Stable.ini" --disable-service-warning >nul 2>&1
 
-copy "C:\Windows\Evolve\Utilities\Service Builder\Stable Services.lnk" "C:\" >nul 2>&1
-del "C:\Windows\Evolve\Utilities\Service Builder\Stable Services.lnk" >nul 2>&1
+REM Wait for file creation
+timeout /t 3 /nobreak >nul
 
+REM Call sanitizing batch in Misc folder
+call "C:\Windows\Misc\Service-Disable-Sanitize.bat"
+
+REM Optional: open build folder
 explorer "C:\Windows\Evolve\Utilities\Service Builder\build"
 
 echo.
